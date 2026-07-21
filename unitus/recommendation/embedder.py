c074f3cc8b130b0dd70c8241a4f0ae63b062978b
Ali Azadi <aliazadicode@gmail.com>
2026-07-21 20:52:58 +0330

    Implemented TextEmbedder class

---
+++ unitus/recommendation/embedder.py
@@ -0,0 +1,19 @@
+from sentence_transformers import SentenceTransformer
+
+class TextEmbedder:
+
+    def __init__(self, model_name="intfloat/multilingual-e5-small"):
+        self.model = SentenceTransformer(model_name)
+    
+    def embed(self, text):
+        return self.model.encode(
+            f"passage: {text}",
+            normalize_embeddings=True
+        )
+
+    def embed_batch(self, texts):
+        passages = [f"passage: {t}" for t in texts]
+        return self.model.encode(
+            passages,
+            normalize_embeddings=True
+        )
